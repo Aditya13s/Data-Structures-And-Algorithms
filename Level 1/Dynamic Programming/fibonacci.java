457b2acce88cc73ffcd4985c1e469001e2c12a57
Aditya <adityasharma131003@gmail.com>
2022-01-10 00:18:37 +0530

    modified file

--- Level 1/Dynamic Programming/fibonacci.java
+++ Level 1/Dynamic Programming/fibonacci.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
 public class fibonacci {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {  
     Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] q = new int[n+1];
