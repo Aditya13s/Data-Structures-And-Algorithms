ccd407e722c1a3d835008f2ff736f94b02589d1d
Aditya <adityasharma131003@gmail.com>
2022-01-10 19:31:54 +0530

    modified files

--- Level 1/Dynamic Programming/fibonacci_1.java
+++ Level 1/Dynamic Programming/fibonacci_1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class fibonacci {
+public class fibonacci_1 {
     public static void main(String[] args) {  
     Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
